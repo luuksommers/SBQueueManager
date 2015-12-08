6b7ad9bb8b4b268e3d3c1494158f4205bf6141e8
luuk <luuk.sommers@meteogroup.com>
2015-12-08 21:55:30 +0100

    Add ConnectionString History

--- src/SBQueueManager/ViewModels/ConnectionStringViewModel.cs
+++ src/SBQueueManager/ViewModels/ConnectionStringViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using Caliburn.Micro;
@@ -13,8 +16,20 @@ namespace SBQueueManager.ViewModels
     {
         private readonly ShellViewModel _shellViewModel;
         private string _message;
-        public string ConnectionString { get; set; }
+        private string _connectionString;
 
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+            set
+            {
+                if (value == _connectionString) return;
+                _connectionString = value;
+                NotifyOfPropertyChange(() => ConnectionString);
+            }
+        }
+
+        public BindableCollection<string> History { get; set; }
         public string Message
         {
             get { return _message; }
@@ -36,11 +51,43 @@ namespace SBQueueManager.ViewModels
         {
             _shellViewModel = shellViewModel;
             ConnectionString = Settings.Default.ConnectionString;
+            if (Settings.Default.History == null)
+            {
+                Settings.Default.History = new StringCollection();
+            }
+            History = new BindableCollection<string>(Settings.Default.History.Cast<string>());
+        }
+
+        public async void Delete(string connectionString)
+        {
+            var metroWindow = (Application.Current.MainWindow as MetroWindow);
+            var metroDialogSettings = new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "OK",
+                NegativeButtonText = "CANCEL",
+                AnimateHide = true,
+                AnimateShow = true,
+                ColorScheme = MetroDialogColorScheme.Theme,
+            };
+            
+            var result = await metroWindow.ShowMessageAsync("Delete", "Are you sure?", MessageDialogStyle.AffirmativeAndNegative, metroDialogSettings);
+            if (result == MessageDialogResult.Affirmative)
+            {
+                Settings.Default.History.Remove(connectionString);
+                History.Remove(connectionString);
+                Settings.Default.Save();
+            }
         }
 
         public async void Set()
         {
             Settings.Default.ConnectionString = ConnectionString;
+            if (!Settings.Default.History.Contains(ConnectionString)) 
+            {
+                Settings.Default.History.Add(ConnectionString);
+                History.Add(ConnectionString);
+            }
+            
             Settings.Default.Save();
 
             var metroWindow = (Application.Current.MainWindow as MetroWindow);
